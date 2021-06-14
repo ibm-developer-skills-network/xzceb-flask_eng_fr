0086204b42e7a499a804cf0d33d3dd5d6ce4f85d
Lavanya Thiruvali Sunderarajan <lavanyas@skillup.tech>
2021-06-14 13:02:59 +0530

    Added skelteon structure

---
+++ final_project/server.py
@@ -0,0 +1,24 @@
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToSpanish():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    return "Translated text to French"
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    return "Translated text to English"
+
+@app.route("/")
+def renderIndexPage():
+    # Write the code to render template
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
