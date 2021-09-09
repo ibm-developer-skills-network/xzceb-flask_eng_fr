84b7c6a57d48527a93cb7673e471692d80b708ae
lvallejomendez <lvallejomendez@gmail.com>
2021-09-09 17:55:28 +0000

    Temporary changes

---
+++ final_project/machinetranslation/prueba.py
@@ -0,0 +1,4 @@
+from translator import english_to_french, french_to_english
+
+print(english_to_french('we are the champions'))
+print(french_to_english('le chat'))
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,44 +1,42 @@
-import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-version = '2018-05-01'
+VERSION = '2018-05-01'
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version=version,
+    version=VERSION,
     authenticator=authenticator
 )
 
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
+def english_to_french(text1):
     #write the code here
     """
     This function translates english to french
     """
     translation = language_translator.translate(
-        text=englishText,
+        text=text1,
         model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
+    french_text = translation['translations'][0]['translation']
 
-    return frenchText
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(text1):
     #write the code here
     """
     This function translates french to english
     """
     translation = language_translator.translate(
-        text=frenchText,
+        text=text1,
         model_id='fr-en').get_result()
-    englishText = translation['translations'][0]['translation']
+    english_text = translation['translations'][0]['translation']
 
-    return englishText
-    
+    return english_text
