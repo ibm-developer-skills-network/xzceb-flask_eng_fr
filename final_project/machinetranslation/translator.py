ca195cfc0a819baadb49f704ecf07df7185c35bb
lvallejomendez <lvallejomendez@gmail.com>
2021-09-09 15:39:06 +0000

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,10 +8,11 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+version = '2018-05-01'
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
+    version=version,
     authenticator=authenticator
 )
 
@@ -19,4 +20,25 @@ language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
     #write the code here
+    """
+    This function translates english to french
+    """
+    translation = language_translator.translate(
+        text=englishText,
+        model_id='en-fr').get_result()
+    frenchText = translation['translations'][0]['translation']
+
     return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    """
+    This function translates french to english
+    """
+    translation = language_translator.translate(
+        text=frenchText,
+        model_id='fr-en').get_result()
+    englishText = translation['translations'][0]['translation']
+
+    return englishText
+    
