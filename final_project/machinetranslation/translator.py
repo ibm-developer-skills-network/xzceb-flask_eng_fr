b2dab628b6edaa4c8e27f0e5c9979d684b361c7b
lvallejomendez <lvallejomendez@gmail.com>
2021-09-09 12:10:52 +0000

    Temporary changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,18 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
