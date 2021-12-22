5ca50cae7243c790495e0882279ff7d67a2cecca
AgentNotime <95002252+AgentNotime@users.noreply.github.com>
2021-12-22 04:36:12 +0100

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,21 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
+    translation = translator.english_to_french(textToTranslate)
     # Write your code here
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
+    translation = translator.french_to_english(textToTranslate)
     # Write your code here
     return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
+    return render_template('index.html')
     # Write the code to render template
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, port=8080)
