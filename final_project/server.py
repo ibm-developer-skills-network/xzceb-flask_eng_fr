cbaf3291f3192e55c8a76134dedada1f498cc21e
adityaroy10 <69969576+adityaroy10@users.noreply.github.com>
2022-08-15 19:41:04 +0530

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -18,7 +18,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
