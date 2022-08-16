71294a8b949bda568f6abe7c77efe336bcc6b1d5
w3slyvg_IBM <wvg@domain.com>
2022-08-16 15:50:11 +0000

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,21 @@
+'''This module is used to test the translator.py script'''
+import unittest
+from translator import english_to_french, french_to_english
+
+class TestE2F(unittest.TestCase):
+    '''class TestE2F to initiate the test from English to French'''
+    def test1(self):
+        '''test the translation from English to French'''
+        self.assertEqual(english_to_french("Book"),"Livre")
+        self.assertNotEqual(english_to_french(0),0)
+        self.assertEqual(english_to_french("Hello"),"Bonjour")
+
+class TestF2E(unittest.TestCase):
+    '''class TestF2E to initiate the test from French to English'''
+    def test2(self):
+        '''test the translation from French to English'''
+        self.assertEqual(french_to_english("Aigle"),"Eagle")
+        self.assertNotEqual(french_to_english(0),0)
+        self.assertEqual(french_to_english("Bonjour"),"Hello")
+
+unittest.main()
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,36 @@
+'''This module is used to translate languages'''
+#import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    '''function to translate from English to French'''
+    translation = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    french_text=translation['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    '''function to translate from French to English'''
+    translation = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    english_text=translation['translations'][0]['translation']
+    return english_text
