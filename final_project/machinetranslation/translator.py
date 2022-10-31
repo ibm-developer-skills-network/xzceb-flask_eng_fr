61ac1996628b302640286b65a53c8f5a9d50e05b
S1rC0c0 <corentin.guichard@gmail.com>
2022-10-31 17:24:11 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -20,15 +20,13 @@ language_translator.set_service_url('https://api.us-east.language-translator.wat
 
 def english_to_french(english_text):
     """ function to translate from english to french"""
-    french_text = language_translator.translate(
-    text=english_text,
-    model_id='en-fr').get_result()
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
     return french_text
 
 def french_to_english(french_text):
     """ function to translate from french to english """
-    english_text = language_translator.translate(
-    text=french_text,
-    model_id='fr-en').get_result()
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
     return english_text
   
--- final_project/server.py
+++ final_project/server.py
@@ -15,10 +15,11 @@ def french_to_english():
     textToTranslate = request.args.get('textToTranslate')
     english_text= translator.french_to_english(textToTranslate)
     return english_text
+    
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
