1a171ec905d81e3a97e7869ecf82a0c3129b0ca1
chlas86 <122174171+chlas86@users.noreply.github.com>
2023-01-08 20:11:19 +0100

    render page

--- final_project/machinetranslation/__init__.py
+++ final_project/machinetranslation/__init__.py
@@ -5,3 +5,5 @@ Created on Sun Jan  8 19:29:57 2023
 @author: claes_z2wsugl
 """
 
+from .translator import englishToFrench
+from .translator import frenchToEnglish
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,21 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
-import json
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    textToTranslate = request.args.get('textToTranslate') 
+    return translator.englishToFrench(textToTranslate)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    textToTranslate = request.args.get('textToTranslate')    
+    return translator.frenchToEnglish(textToTranslate)
 
-@app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+@app.route("/", methods = ['GET'])
+def renderIndexPage():    
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
