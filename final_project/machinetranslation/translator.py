c1caaf1e32bad3cfd4749ced0f34112bf39a53ae
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-02-18 09:32:12 +0530

    Created a translator using IBM cloud watson api

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,45 @@
+"""
+This module provides functions for translating text
+between English and French using the IBM Cloud Watson Translator API.
+"""
+
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+load_dotenv()
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+    """
+    Translates English text to French using the IBM Cloud Watson Translator API.
+    """
+    translation = language_translator.translate(
+        text=englishText,
+        source='en',
+        target='fr'
+    ).get_result()
+    frenchText = translation['translations'][0]['translation']
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    """
+    Translates French text to English using the IBM Cloud Watson Translator API.
+    """
+    translation = language_translator.translate(
+        text=frenchText,
+        source='fr',
+        target='en'
+    ).get_result()
+
+    englishText = translation['translations'][0]['translation']
+    return englishText
