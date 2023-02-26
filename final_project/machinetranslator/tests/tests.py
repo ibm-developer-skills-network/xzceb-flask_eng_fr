fb37b6043619b18f15f742c3c22bbefa612aabd9
tomekpavel <tomek.pavel@hotmail.com>
2023-02-26 18:10:14 +0100

    final changes

---
+++ final_project/machinetranslator/tests/tests.py
@@ -0,0 +1,22 @@
+"""Unit tests for translator.py methods"""
+
+import unittest
+from ..translator import english_to_french, french_to_english
+
+
+class TestTranslator(unittest.TestCase):
+    """TestCase for translator.py unit tests"""
+    def test_english_to_french(self):
+        """Tests whether English translator works correctly on basic words"""
+        self.assertEqual(english_to_french('dog').lower(), 'chien')
+        self.assertNotEqual(english_to_french('dog').lower(), 'dog')
+
+
+    def test_french_to_english(self):
+        """Tests whether French translator works correctly on basic words"""
+        self.assertEqual(french_to_english('chien').lower(), 'dog')
+        self.assertNotEqual(french_to_english('chien').lower(), 'chien')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,30 @@
-from machinetranslation import translator
-from flask import Flask, render_template, request
+
 import json
+from flask import Flask, render_template, request
+
+import machinetranslator
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    french_text = machinetranslator.translator.english_to_french(
+        english_text=textToTranslate
+    )
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    english_text = machinetranslator.translator.french_to_english(
+        french_text=textToTranslate
+    )
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
