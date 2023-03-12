c69b11675e69bdebae9203aca8d755173492d969
devops2795 <devops2795@gmail.com>
2023-03-12 13:32:51 -0400

    machine Translator

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,10 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
