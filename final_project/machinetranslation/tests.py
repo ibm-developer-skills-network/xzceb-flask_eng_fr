3e00753a0a5351b033ea921d8862e9cae76a87e3
lvallejomendez <lvallejomendez@gmail.com>
2023-03-13 17:06:21 -0400

    tests updated

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,17 +3,35 @@ import unittest
 from translator import english_to_french, french_to_english
 
 class TestE2F(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(english_to_french(' '), ' ') # test for null input for english_to_french
-        self.assertEqual(english_to_french('Hello'), 'Bonjour') # test for the translation of the english word
-        self.assertNotEqual(english_to_french('Hello'), 'Hello') # test for the translation of the english word
+    """Test cases for english_to_french"""
+
+    def test_E2F_null(self):
+        """Test for null input for english_to_french"""
+        self.assertEqual(english_to_french(' '), ' ')
+
+    def test_E2F_translation(self):
+        """Test for the translation of the english word"""
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_E2F_not_translation(self):
+        """Test for the not translation of the english word"""
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
         
 
-class TestF2E(unittest.TestCase):
-    def test2(self):
-        self.assertEqual(french_to_english(' '), ' ') # test for null input for french_to_english
-        self.assertEqual(french_to_english('Bonjour'), 'Hello') # test for the translation of the french word
-        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour') # test for the translation of the french word
 
+class TestF2E(unittest.TestCase):
+    """Test cases for french_to_english"""
+
+    def test_F2E_null(self):
+        """Test for null input for french_to_english"""
+        self.assertEqual(french_to_english(' '), ' ')
+    
+    def test_F2E_null(self):
+        """Test for the translation of the french word"""
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+    
+    def test_F2E_null(self):
+        """Test for the not translation of the french word"""
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
 
 unittest.main()
