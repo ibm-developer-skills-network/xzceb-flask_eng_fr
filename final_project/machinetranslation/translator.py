42d5e78fd468fb87924d020a8a3e56dc719e6d37
lvallejomendez <lvallejomendez@gmail.com>
2023-03-13 16:39:09 -0400

    translator updated

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
@@ -9,33 +9,31 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 VERSION = '2018-05-01'
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator('{apikey}')
 language_translator = LanguageTranslatorV3(
-    version=VERSION,
+    version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url(url)
+language_translator.set_service_url('{url}')
 
-def english_to_french(text1):
-    #write the code here
+def english_to_french(englishText):
     """
     This function translates english to french
     """
     translation = language_translator.translate(
-        text=text1,
+        text=englishText,
         model_id='en-fr').get_result()
     french_text = translation['translations'][0]['translation']
 
     return french_text
 
-def french_to_english(text1):
-    #write the code here
+def french_to_english(frenchText):
     """
     This function translates french to english
     """
     translation = language_translator.translate(
-        text=text1,
+        text=frenchText,
         model_id='fr-en').get_result()
     english_text = translation['translations'][0]['translation']
 
