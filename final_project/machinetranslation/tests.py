b9a8830293381fc38eef2ec1b24caacf783255aa
your_github_username <kukushkin.cv@gmail.com>
2023-03-23 16:57:51 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -9,16 +9,16 @@ class TestTranslator(unittest.TestCase):
         french_text = english_to_french(english_text)
         self.assertEqual(french_text, "Bonjour")
 
-    def test_french_to_english_hello(self):
-        french_text = "Bonjour"
-        english_text = french_to_english(french_text)
-        self.assertEqual(english_text, "Hello")
-
     def test_english_to_french_au_revoir(self):
         english_text = "Goodbye"
         french_text = english_to_french(english_text)
         self.assertEqual(french_text, "Au revoir")
 
+    def test_french_to_english_hello(self):
+        french_text = "Bonjour"
+        english_text = french_to_english(french_text)
+        self.assertEqual(english_text, "Hello")
+        
     def test_french_to_english_goodbye(self):
         french_text = "Au revoir"
         english_text = french_to_english(french_text)
