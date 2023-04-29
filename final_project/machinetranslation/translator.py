4f0ac4262d31aa3b3af1ebdd912a974695967166
Caliperti1 <Christopher.aliperti@gmail.com>
2023-04-29 09:41:13 -0400

    created init file

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,14 +1,14 @@
+# Import 
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
+# Read environment variables
 load_dotenv()
-
 apikey = os.environ['apikey']
 url = os.environ['url']
-
 
 # Create instance of IBM Watson Language Translator 
 authenticator = IAMAuthenticator(apikey)
@@ -16,12 +16,15 @@ language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
-
 language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
 
 # English to French function
 def englishToFrench(englishText):
-    
+    ''' 
+    This function takes a string in english and returns a string in french using 
+    the IBM Watson Language Translator API
+    '''
+   
     translation = language_translator.translate(
     text=englishText,
     model_id='en-fr').get_result()
@@ -32,11 +35,15 @@ def englishToFrench(englishText):
 
 # French to English Function
 def frenchToEnglish(FrenchText):
+    ''' 
+    This function takes a string in french and returns a string in english using 
+    the IBM Watson Language Translator API
+    '''
 
     translation = language_translator.translate(
     text=FrenchText,
     model_id='fr-en').get_result()
-    
+
     englishText  = translation['translations'][0]['translation']
-        
+
     return englishText
