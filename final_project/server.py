afaa83324ba359aa2e667b209ccd6f7b851fc90b
Caliperti1 <Christopher.aliperti@gmail.com>
2023-04-29 09:53:36 -0400

    added package to server.py

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    translator.englishToFrench(textToTranslate)
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    translator.frenchToEnglish(textToTranslate)
     return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
