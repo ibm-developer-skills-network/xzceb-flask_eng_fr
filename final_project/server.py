31888a4e6187d0cb7176bf01770099da2493ce7b
Camh-git <74473559+Camh-git@users.noreply.github.com>
2023-05-05 21:00:42 +0100

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,5 +1,6 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
+import machinetranslation
 import json
 
 app = Flask("Web Translator")
@@ -7,18 +8,22 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    response = translator.english_to_french(textToTranslate)
+    translation = response.get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    response = translator.french_to_english(textToTranslate)
+    translation = response.get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__Main__':
     app.run(host="0.0.0.0", port=8080)
