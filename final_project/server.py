6a684d9551bfc6d1745cb1ea0b213eeda0e220e4
farebrad <35327264+farebrad@users.noreply.github.com>
2023-05-17 17:22:44 -0400

    translate functions were the wrong way round

--- final_project/server.py
+++ final_project/server.py
@@ -7,13 +7,13 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    english_text = translator.french_to_english(textToTranslate)
+    english_text = translator.english_to_french(textToTranslate)
     return english_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    french_text = translator.english_to_french(textToTranslate)
+    french_text = translator.french_to_english(textToTranslate)
     return french_text
 
 @app.route("/")
