53402182297b035f9a61f8315a9330500ce8816f
MohannadAKassar <Mohakassar1998@gmail.com>
2023-06-17 23:40:37 +0300

    Updated

---
+++ Application/server.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, request
+import json
+from machinetranslation import translator
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    french_text = translator.english_to_french(textToTranslate)
+    return french_text
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    english_text = translator.french_to_english(textToTranslate)
+    return english_text
+
+@app.route("/")
+def renderIndexPage():
+    
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
