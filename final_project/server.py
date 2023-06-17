cd473fc7f0f6810a545ada3d30ec3e95c47d3582
MohannadAKassar <127568955+MohannadAKassar@users.noreply.github.com>
2023-06-17 22:44:40 +0300

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,8 +1,6 @@
 
 from flask import Flask, render_template, request
 import json
-package_folder = os.path.abspath(os.path.join(os.path.dirname(__machinetranslation__), 'machinetransaltion'))
-sys.path.insert(0, package_folder)
 from machinetranslation import translator
 
 app = Flask("Web Translator")
@@ -10,13 +8,13 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    french_text = english_to_french(textToTranslate)
+    french_text = translator.english_to_french(textToTranslate)
     return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    english_text = french_to_english(textToTranslate)
+    english_text = translator.french_to_english(textToTranslate)
     return english_text
 
 @app.route("/")
