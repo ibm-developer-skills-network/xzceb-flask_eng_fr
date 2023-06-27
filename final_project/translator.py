bbb977ef53c71ee06fabf6bf85e079300957413f
gtwilcocks <gtwilcocks@yahoo.com>
2023-06-27 10:38:40 -0400

    test2

---
+++ final_project/translator.py
@@ -0,0 +1,11 @@
+from deep_translator import MyMemoryTranslator
+
+def englishToFrench(englishText):
+    #write the code here
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    return englishText
+
+    #test
