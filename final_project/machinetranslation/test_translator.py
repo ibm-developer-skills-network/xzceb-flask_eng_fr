90a457e938b87040462a0babfc1539d6bb5027c0
your_github_username <youremail@domain.com>
2023-06-30 17:10:16 -0400

    Fixed the Test issues

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -2,13 +2,25 @@ import unittest
 from translator import englishToFrench, frenchToEnglish
 
 class TestEF(unittest.TestCase):
-    def testOne(self):
+       def test1(self):
+        # Test Hello returns Bonjour
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        # Test Hello does not return Hello
         self.assertNotEqual(englishToFrench('Hello'), 'Hello')
+        # Test None returns empty string
+        self.assertNotEqual(englishToFrench("None"), '')
+        # Test empty string returns empty string
+        self.assertNotEqual(englishToFrench(0), 0)
 
 class TestFE(unittest.TestCase):
-    def testTwo(self):
+    def test1(self):
+        # Test Hello returns Bonjour
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        # Test Hello does not return Hello
         self.assertNotEqual(frenchToEnglish('Bonjour'), 'Bonjour')
+        # Test None returns empty string
+        self.assertNotEqual(frenchToEnglish("None"), '')
+        # Test empty string returns empty string
+        self.assertNotEqual(frenchToEnglish(0), 0)
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,22 +1,23 @@
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from deep_translator import MyMemoryTranslator
 
-authenticator = IAMAuthenticator("nA_3AG9jCGWTOl-o5nXCoyGkZYNGMZeLhRP_ULwl4RW8")
+authenticator = IAMAuthenticator('nA_3AG9jCGWTOl-o5nXCoyGkZYNGMZeLhRP_ULwl4RW8')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
  )
-language_translator.set_service_url("https://api.us-south.language-translator.watson.cloud.ibm.com/instances/9ac5757f-278e-448e-99d6-cb25d97fdb66")
+language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/9ac5757f-278e-448e-99d6-cb25d97fdb66')
 
-def englishToFrench(englishText):
-    frenchText = MyMemoryTranslator(source='en', target='fr').translate(englishText)
-    print(frenchText)
-    return frenchText
+def englishToFrench(english_text):
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
-def frenchToEnglish(frenchText):
-    englishText = MyMemoryTranslator(source='fr', target='en').translate(frenchText)
-    print(englishText)
-    return englishText
+def frenchToEnglish(french_text):
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
+
