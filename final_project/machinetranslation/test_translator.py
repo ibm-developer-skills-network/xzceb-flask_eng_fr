6179a4e32ae1daf81783bfe084cfc3ec82596094
your_github_username <youremail@domain.com>
2023-06-30 18:19:51 -0400

    Temporary changes

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -1,26 +1,26 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TestEF(unittest.TestCase):
        def test1(self):
         # Test Hello returns Bonjour
-        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
         # Test Hello does not return Hello
-        self.assertNotEqual(englishToFrench('Hello'), 'Hello')
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
         # Test None returns empty string
-        self.assertNotEqual(englishToFrench("None"), '')
+        self.assertNotEqual(english_to_french("None"), '')
         # Test empty string returns empty string
-        self.assertNotEqual(englishToFrench(0), 0)
+        self.assertNotEqual(english_to_french(0), 0)
 
 class TestFE(unittest.TestCase):
     def test1(self):
         # Test Hello returns Bonjour
-        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
         # Test Hello does not return Hello
-        self.assertNotEqual(frenchToEnglish('Bonjour'), 'Bonjour')
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
         # Test None returns empty string
-        self.assertNotEqual(frenchToEnglish("None"), '')
+        self.assertNotEqual(french_to_english("None"), '')
         # Test empty string returns empty string
-        self.assertNotEqual(frenchToEnglish(0), 0)
+        self.assertNotEqual(french_to_english(0), 0)
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,7 +2,6 @@ import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from deep_translator import MyMemoryTranslator
 
 authenticator = IAMAuthenticator('nA_3AG9jCGWTOl-o5nXCoyGkZYNGMZeLhRP_ULwl4RW8')
 language_translator = LanguageTranslatorV3(
@@ -11,13 +10,14 @@ language_translator = LanguageTranslatorV3(
  )
 language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/9ac5757f-278e-448e-99d6-cb25d97fdb66')
 
-def englishToFrench(english_text):
+def english_to_french(english_text):
+    '''English to French'''
     translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
     french_text = translation['translations'][0]['translation']
     return french_text
 
-def frenchToEnglish(french_text):
+def french_to_english(french_text):
+    '''French to English'''
     translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
     english_text = translation['translations'][0]['translation']
     return english_text
-
