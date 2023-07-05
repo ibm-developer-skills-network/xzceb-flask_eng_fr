9a6278843d24e6284cf9b958c4e105e2059060af
pxc <105520102+nocaprc@users.noreply.github.com>
2023-07-05 10:41:49 +0530

    updated again

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,13 +2,13 @@ from deep_translator import MyMemoryTranslator
 
 def english_to_french(english_text):
     'Is this working?'
-    translated = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+    translated = MyMemoryTranslator(source='en-IN', target='fr-FR').translate(english_text)
     print(translated)
     return translated
 
 
 def french_to_english(french_text):
     'Is this working?'
-    translated = MyMemoryTranslator(source='fr', target='en').translate(french_text)
+    translated = MyMemoryTranslator(source='fr-FR', target='en-IN').translate(french_text)
     print(translated)
     return translated
