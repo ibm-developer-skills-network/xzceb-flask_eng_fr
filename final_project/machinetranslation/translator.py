10c7c38a6f5b13d3cc4fe870803c135460bbcb25
skirtane <ksuraaj@gmail.com>
2023-07-10 09:17:57 -0400

    Added translation functions

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,14 @@
+''' Import the MyMemoryTranslator from deep_translator'''
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
-    frenchText = MyMemoryTranslator(source="en",
-    target="fr").translate(text=englishText)
-    return frenchText
+def english_to_french(english_text):
+    ''' This function converts english text to french'''
+    french_text = MyMemoryTranslator(source="en",
+    target="fr").translate(text=english_text)
+    return french_text
 
-def frenchToEnglish(frenchText):
-    englishText = MyMemoryTranslator(source="fr",
-    target="en").translate(text=frenchText)
-    return englishText
+def french_to_english(french_text):
+    ''' This function converts text from french to english'''
+    english_text = MyMemoryTranslator(source="fr",
+    target="en").translate(text=french_text)
+    return english_text
