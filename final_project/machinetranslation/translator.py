171fdbdd9838bda5bbd9218b0d6fe33c794d0bfd
RupeshMangalam21 <rupeshm.cse21@gmail.com>
2023-07-27 06:46:58 -0400

    upto task 5-6

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,33 @@
- from deep_translator import MyMemoryTranslator
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
 
-def englishToFrench(englishText):
-    translator = MyMemoryTranslator(source='en', target='fr')
-    frenchText = translator.translate(englishText)
-    return frenchText
+load_dotenv()
 
-def frenchToEnglish(frenchText):
-    translator = MyMemoryTranslator(source='fr', target='en')
-    englishText = translator.translate(frenchText)
-    return englishText
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+
+def english_to_french(english_text):
+    translation = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    french_text = json.dumps(translation, indent=2, ensure_ascii=False)
+    return french_text
+
+def french_to_english(french_text):
+    translation = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    english_text = json.dumps(translation, indent=2, ensure_ascii=False)
+    return english_text
